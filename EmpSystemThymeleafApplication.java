74f16ee67a77bf342475e1c3f15696d9634799b1
Vishal <153981922+Vishalkande26@users.noreply.github.com>
2025-04-15 11:25:18 +0530

    Create EmpSystemThymeleafApplication.java

---
+++ EmpSystemThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSystemThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSystemThymeleafApplication.class, args);
+		System.out.println("wecome to employee system..........");
+	}
+
+}
